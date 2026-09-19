35d879f59a00989710fa00ade22761cf3c31df0b
Yang Xi Gao <36776982+joGao01@users.noreply.github.com>
2026-09-18 22:54:22 -0400

    update site title stuff

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,6 @@
-export const SITE_TITLE = 'I Hope This Blog Finds You Well';
-export const SITE_DESCRIPTION = 'Notes on work, art, and life';
+export const HOME_TITLE = 'I Hope This Finds You Well';
+export const ART_TITLE = 'Art';
+export const ABOUT_TITLE = 'About Me';
+export const BLOG_TITLE = 'I Hope This Blog Finds You Well';
+export const SITE_DESCRIPTION = "Jo's notes on work, art, and life";
 export const POSTS_PER_PAGE = 6;
